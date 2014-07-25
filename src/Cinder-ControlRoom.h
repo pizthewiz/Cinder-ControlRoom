92ac749e47574c37884e699170e4a326e4a073c6
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2014-07-24 19:39:55 -0700

    fix copyright year

--- src/Cinder-ControlRoom.h
+++ src/Cinder-ControlRoom.h
@@ -3,7 +3,7 @@
 //  Cinder-ControlRoom
 //
 //  Created by Jean-Pierre Mouilleseaux on 02 Jul 2014.
-//  Copyright 2013 Chorded Constructions. All rights reserved.
+//  Copyright 2014 Chorded Constructions. All rights reserved.
 //
 
 #pragma once
